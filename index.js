947ca4e567aefe2e0b8f8b77a509a9f578f25104
Alberto Biolchi <169494833+Alberto23B@users.noreply.github.com>
2024-07-26 10:55:12 +0200

    Added nodemon to dev-dependencies

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const app = express()
 const cors = require('cors')
 require('dotenv').config()
 const mongoose = require("mongoose");
+const nodemon = require("nodemon");
+
 
 app.use(cors())
 app.use(express.static('public'))
